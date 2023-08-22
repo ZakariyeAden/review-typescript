d9f5aa4a067b1b092f6fbe82ecdc18085b74b16e
ZakariyeAden <zakariye5417@gmail.com>
2023-08-21 19:12:01 -0500

    functions and enums

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,18 @@ if (age <= 50) {
     age += 10;
     console.log(age);
 }
+let sales = 1234567890;
+let course = 'TypeScript';
+let is_Published = false;
+let level;
+function render(document) {
+    console.log(document);
+}
+let numbers = [6, 5, 6, 6];
+let numbersAny = [];
+let user = [1, 'Zakariye'];
+user.push(1);
+;
+let mySize = 2;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -25,8 +25,34 @@ function render(document: any){
 
 
 // Arrays
-let numbers:number[] = [6,5,6,6,'4'];
+let numbers:number[] = [6,5,6,6];
 // Any: should avoid
 let numbersAny = [];
 // Methods 
 // numbers.forEach(n => n.valueOf())
+
+// Tuples
+// 1, 'Zakariye
+let user: [number, string] = [1, 'Zakariye'];
+user.push(1);
+
+// ENUMS
+
+// const small = 1;
+// const medimum = 2;
+// const large = 3;
+
+const enum Size { Small = 1, Medimum, Large};
+
+let mySize: Size = Size.Medimum;
+console.log(mySize);
+
+// Functions
+function calculateTax(income: number, taxYear: 2022): number{
+  
+  if(taxYear < 2022 )
+    return income * 1.3;
+    return income * 1.3;
+  // Undefined
+}
+calculateTax(10_000,2022);
