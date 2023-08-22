8934520f10a43811d595b2340f2f2153918cc5df
ZakariyeAden <zakariye5417@gmail.com>
2023-08-21 19:33:39 -0500

    nullable

--- src/index.ts
+++ src/index.ts
@@ -104,3 +104,14 @@ type Quantity = 50 | 100;
 let quantity:  Quantity = 100;
 
 type Metric = 'cm' | 'Inch';
+
+
+// Nullable
+function greet(name: string | null | undefined){
+  if(name)
+  console.log(name.toUpperCase());
+  else
+  console.log('Hola');
+}
+
+greet(null)
